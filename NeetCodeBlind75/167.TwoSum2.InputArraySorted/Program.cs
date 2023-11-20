2c7251d0166f1f58eda37ba4cc2216129e7059e5
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-11-20 17:38:24 +0200

    TwoSumTwoWithSortedArrayEasy

---
+++ NeetCodeBlind75/167.TwoSum2.InputArraySorted/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] TwoSum(int[] numbers, int target)
+{
+    int l = 0;
+    int r = numbers.Length - 1;
+
+    while (l < r)
+    {
+        int result = numbers[l] + numbers[r];
+
+        if (result > target)
+        {
+            r--;
+        }
+        else if (result < target)
+        {
+            l++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return new[] { l + 1, r + 1 };
+}
