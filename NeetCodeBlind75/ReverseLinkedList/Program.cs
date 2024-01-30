e56277a270b74e20248ca111cd09e0e462505c69
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-01-31 01:45:59 +0200

    "Implementing a solution for reversing a linked list (#206)
    This commit introduces a solution for the LeetCode problem involving reversing a linked list. The algorithm iterates through the list, reversing the direction of links to achieve the desired outcome. The implementation uses three pointers - 'current,' 'previous,' and 'next' - to traverse and manipulate the linked list efficiently."

---
+++ NeetCodeBlind75/ReverseLinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Iterative approach with pointers
+public class Solution {
+    public ListNode ReverseList(ListNode head)
+    {
+        //This will be used to traverse the list
+        ListNode current = head;
+        //this will defacto represent the new current value
+        ListNode previous = null;
+        //temporary value for saving the link in the list
+        ListNode next;
+
+        while (current != null)
+        {
+            //We save the link to the next node in the list
+            next = current.next;
+
+            //We reverse the direction of our list
+            current.next = previous;
+            //we save our new list to this variable
+            previous = current;
+            //we continue forward with our temporary saved variable
+            current = next;
+        }
+
+        head = previous;
+
+        return head;
+    }
+}
