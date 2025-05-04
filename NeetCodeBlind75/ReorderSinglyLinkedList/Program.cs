9a9f01959235ab489f8921c34384fbd45d492b37
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-05-04 12:04:56 +0300

    Added Solutions for ReorderSinglyLinkedList

---
+++ NeetCodeBlind75/ReorderSinglyLinkedList/Program.cs
@@ -0,0 +1,94 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public void ReorderList(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            // second half start
+            slow = slow.next;
+            // check if end of list is reached
+            fast = fast.next.next;
+        }
+
+        // Reverse second half of list
+        ListNode? prev = null;
+        ListNode? curr = slow;
+
+        while (curr != null)
+        {
+            ListNode? next = curr.next;
+            curr.next = prev;
+            prev = curr;
+
+            curr = next;
+        }
+
+        ListNode? first = head;
+        ListNode? second = prev;
+        while (second != null)
+        {
+            // save references to original list
+            ListNode? temp1 = first.next;
+            ListNode? temp2 = second.next;
+
+            // break links(reorder)
+            first.next = second;
+            second.next = temp1;
+            // move to next node
+            first = temp1;
+            second = temp2;
+        }
+
+        first.next = null;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create the linked list 2 -> 4 -> 6 -> 8
+        ListNode head = new ListNode(2, new ListNode(4, new ListNode(6, new ListNode(8, new ListNode(10)))));
+
+        // Print the original list
+        Console.WriteLine("Original List:");
+        PrintList(head);
+
+        // Reorder the list
+        Solution solution = new Solution();
+        solution.ReorderList(head);
+
+        // Print the reordered list
+        Console.WriteLine("Reordered List:");
+        PrintList(head);
+    }
+
+    public static void PrintList(ListNode head)
+    {
+        ListNode current = head;
+        while (current != null)
+        {
+            Console.Write(current.val);
+            if (current.next != null)
+            {
+                Console.Write(" -> ");
+            }
+            current = current.next;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ NeetCodeBlind75/TaskMaster/Program.cs
@@ -0,0 +1,53 @@
+﻿static int tasks(int n, List<int> a, List<int> b)
+{
+    int[] inDegree;
+    List<int>[] graph = CreateDAP(n, a, b, out inDegree);
+    bool[] visited = new bool[n + 1];
+    Dictionary<int, List<int>> directedGraph = new Dictionary<int, List<int>>();
+
+
+    return 0;
+}
+
+static List<int>[] CreateDAP(int n, List<int> a, List<int> b, out int[] inDegree)
+{
+    Dictionary<int, List<int>> nodeNeighbours = new Dictionary<int, List<int>>();
+    inDegree = new int[n + 1];
+
+    for (int i = 0; i <= n; i++)
+        graph[i] = new List<int>();
+
+    for (int i = 0; i < a.Count; i++)
+    {
+        int from = b[i];
+        int to = a[i];
+
+        graph[from].Add(to);
+        inDegree[to]++;
+    }
+    return graph;
+}
+
+static bool DFSCycleCheck(int node, bool[] visited, List<int>[] graph)
+{
+    Stack<int> dfsStack = new Stack<int>();
+    dfsStack.Push(node);
+
+    while (dfsStack.Count > 0)
+    {
+        int curr = dfsStack.Pop();
+        if (visited[curr]) return true;
+        visited[curr] = true;
+
+        foreach (int neighbour in graph[curr])
+        {
+            dfsStack.Push(neighbour);
+        }
+    }
+
+    return false;
+}
+List<int> a = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+List<int> b = new List<int> { 9, 8, 10, 4, 9, 1, 3, 5, 9 };
+
+Console.WriteLine(tasks(10, a, b));
